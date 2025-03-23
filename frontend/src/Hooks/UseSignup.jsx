dbd8128c022f3a3c2865470b914ca6ea9a7487da
Kimara02 <151428771+Kimara02@users.noreply.github.com>
2025-03-23 12:56:19 +0545

    Update UseSignup.jsx

--- frontend/src/Hooks/UseSignup.jsx
+++ frontend/src/Hooks/UseSignup.jsx
@@ -12,7 +12,7 @@ export const useSignup = () => {
       setError('null')
       
       
-        const response = await fetch('http://localhost:4000/api/users/signup', {
+        const response = await fetch('https://workoutbuddy-backend-kx51.onrender.com/api/users/signup', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
