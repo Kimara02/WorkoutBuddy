8805cc53ae68280bc7637526825ac2b32d2e105d
Kimara02 <151428771+Kimara02@users.noreply.github.com>
2025-03-23 12:57:09 +0545

    Update Home.jsx

--- frontend/src/Routes/Home/Home.jsx
+++ frontend/src/Routes/Home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   useEffect(() => {
     const fetchWorkouts = async () => {
       try {
-        const response = await fetch( 'http://localhost:4000/api/kimara',{
+        const response = await fetch( 'https://workoutbuddy-backend-kx51.onrender.com/api/kimara',{
           headers:{
             'Authorization':`Bearer ${user.token}`,
           },
